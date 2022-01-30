2ca637bbd84b7971c344df2c0e2f8f39fa7d6c93
BorisPlaceholder <98130919+BorisPlaceholder@users.noreply.github.com>
2022-01-30 22:20:35 +0100

    Create tetsucode_test.py

---
+++ tetsucode_test.py
@@ -0,0 +1,5 @@
+def test_tetsucabrafunction():
+  assert main.tetsucabrafunction('TETSUCABRA')
+  assert main.tetsucabrafunction('NOTTETSU')
+  assert main.tetsucabrafunction('e')
+  assert main.tetsucabrafunction('E')
