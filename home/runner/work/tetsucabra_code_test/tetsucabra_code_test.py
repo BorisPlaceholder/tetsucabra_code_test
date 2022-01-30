517d982b670241549bf8ab8388f5ac3e5abd5a44
BorisPlaceholder <98130919+BorisPlaceholder@users.noreply.github.com>
2022-01-30 22:14:06 +0100

    Create tetsucabra_code_test.py

---
+++ home/runner/work/tetsucabra_code_test/tetsucabra_code_test.py
@@ -0,0 +1,6 @@
+import main
+def test_tetsucabrafunction():
+  assert main.tetsucabrafunction('TETSUCABRA')
+  assert main.tetsucabrafunction('NOTTETSU')
+  assert main.tetsucabrafunction('e')
+  assert main.tetsucabrafunction('E')
