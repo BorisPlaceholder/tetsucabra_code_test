ea9f3c78128b50d1cfc1c21dd75887f22a9b733e
BorisPlaceholder <98130919+BorisPlaceholder@users.noreply.github.com>
2022-01-30 22:04:51 +0100

    Update tetsucode.py

--- tetsucode.py
+++ tetsucode.py
@@ -8,10 +8,3 @@ def tetsucabrafunction(tetsutest):
       tetsutestvar=1
     else:
       print(":(")
-
-import main
-def test_tetsucabrafunction():
-  assert main.tetsucabrafunction('TETSUCABRA')
-  assert main.tetsucabrafunction('NOTTETSU')
-  assert main.tetsucabrafunction('e')
-  assert main.tetsucabrafunction('E')
