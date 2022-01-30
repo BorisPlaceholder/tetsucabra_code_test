20d78615670bc2292a080ff073a80a57e7a542a1
BorisPlaceholder <98130919+BorisPlaceholder@users.noreply.github.com>
2022-01-30 22:04:00 +0100

    Update tetsucode.py

--- tetsucode.py
+++ tetsucode.py
@@ -1,9 +1,17 @@
-tetsutestvar=0
-while tetsutestvar==0:
-  print("TETSUCABRA?")
-  tetsutest=input()
-  if tetsutest.lower()=="tetsucabra":
-    print("TETSUCABRA!")
-    tetsutestvar=1
-  else:
-    print(":(")
+def tetsucabrafunction(tetsutest):
+  tetsutestvar=0
+  while tetsutestvar==0:
+    print("TETSUCABRA?")
+    tetsutest=input()
+    if tetsutest.lower()=="tetsucabra":
+      print("TETSUCABRA!")
+      tetsutestvar=1
+    else:
+      print(":(")
+
+import main
+def test_tetsucabrafunction():
+  assert main.tetsucabrafunction('TETSUCABRA')
+  assert main.tetsucabrafunction('NOTTETSU')
+  assert main.tetsucabrafunction('e')
+  assert main.tetsucabrafunction('E')
