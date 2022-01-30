b1a3aec4671370c64e6b9f3c3f6582cc2fcd6e37
BorisPlaceholder <98130919+BorisPlaceholder@users.noreply.github.com>
2022-01-30 22:19:45 +0100

    Delete tetsucabra_code_test.py

--- tetsucabra_code_test.py
+++
@@ -1,6 +0,0 @@
-import main
-def test_tetsucabrafunction():
-  assert main.tetsucabrafunction('TETSUCABRA')
-  assert main.tetsucabrafunction('NOTTETSU')
-  assert main.tetsucabrafunction('e')
-  assert main.tetsucabrafunction('E')
