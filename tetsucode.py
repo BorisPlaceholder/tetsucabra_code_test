ab5e3deaa5b18d869cece129511ee871fd35d69b
BorisPlaceholder <98130919+BorisPlaceholder@users.noreply.github.com>
2022-01-30 21:50:21 +0100

    Create tetsucode.py

---
+++ tetsucode.py
@@ -0,0 +1,9 @@
+tetsutestvar=0
+while tetsutestvar==0:
+  print("TETSUCABRA?")
+  tetsutest=input()
+  if tetsutest.lower()=="tetsucabra":
+    print("TETSUCABRA!")
+    tetsutestvar=1
+  else:
+    print(":(")
