6175ffea4bb36c251bf3e3e6e4e8b01e4d8735ca
BorisPlaceholder <98130919+BorisPlaceholder@users.noreply.github.com>
2022-01-30 22:37:32 +0100

    Update tetsucode.py

--- tetsucode.py
+++ tetsucode.py
@@ -2,9 +2,9 @@ def tetsucabrafunction(tetsutest):
   tetsutestvar=0
   while tetsutestvar==0:
     print("TETSUCABRA?")
-    tetsutest=input()
     if tetsutest.lower()=="tetsucabra":
       print("TETSUCABRA!")
       tetsutestvar=1
     else:
       print(":(")
+      tetsutestvar=1
